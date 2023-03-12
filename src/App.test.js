4f966b054e291a96bab1ee8cd3d99620fc0f132e
Furqan Hameed <furqancode@gmail.com>
2023-03-12 15:40:13 +0530

    Added test cases

---
+++ src/App.test.js
@@ -0,0 +1,5 @@
+import { describe, it, expect } from 'vitest';
+
+it("renders without crashing", () => {
+  expect(1).toEqual(1)
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import type { UserConfig as VitestUserConfigInterface } from 'vitest/config';
 
 // https://vitejs.dev/config/
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+});
